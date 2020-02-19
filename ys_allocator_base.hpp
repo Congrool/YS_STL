c64a4824e8a206bc57a87ffaeb219a5f1a2c166b
ys <783928876@qq.com>
2020-02-19 22:56:28 +0800

    add allocator and global construct

---
+++ ys_allocator_base.hpp
@@ -0,0 +1,97 @@
+#ifndef _YS_ALLOCATOR_BASE_H
+#define _YS_ALLOCATOR_BASE_H
+
+#include<bits/c++config.h>
+
+namespace YS_STL{
+
+  using std::size_t;
+  using std::ptrdiff_t;
+  
+  void __throw_bad_alloc(){
+      std::cerr<<"out of memory" << std::endl; 
+      exit(1);
+  }
+
+  template<typename _Tp>
+  class __allocator_base{
+    public:
+        typedef size_t      size_type;
+        typedef _Tp*        pointer;
+        typedef const _Tp*  const_pointer;
+        typedef _Tp&        reference;
+        typedef const _Tp&  const_reference;
+        typedef _Tp         value_type;
+        typedef ptrdiff_t   difference_type;
+
+        template<typename _Tp1>
+        struct rebind{
+            typedef __allocator_base<_Tp1> other;
+        };
+
+        // normal constructor
+        __allocator_base() noexcept { };
+
+        // normal copy constructor
+        __allocator_base(const __allocator_base<_Tp>&) noexcept { };
+
+        // copy assignment constructor
+        __allocator_base& operator=(const __allocator_base<_Tp>&) noexcept { };
+
+        // deallocator
+        ~__allocator_base() noexcept { };
+
+
+        pointer address(reference _x) noexcept{
+            return &_x;
+        }
+        const_pointer address(reference _x) const noexcept{
+            return &_x;
+        }
+
+        // allocate uninitialized memory
+        pointer allocate(size_type _n, const void* = 0){
+            if(_n > max_size())
+                YS_STL::__throw_bad_alloc();
+            return static_cast<_Tp*>(::operator new(_n * sizeof(_Tp)));
+        }
+
+        void deallocate(pointer _p, size_type){
+            ::operator delete(_p);
+        }
+
+        // max memory the machine has
+        size_type max_size() const noexcept{
+            return size_t(-1) / sizeof(_Tp);
+        }
+
+        template<typename _Up, typename... _Args>
+        void contruct(_Up* _p, _Args&&... _args){
+            ::new((void*)_p) _Up(std::forward<_Args>(_args)...);
+        }
+
+        template<typename _Up>
+        void destory(_Up* _p){
+            _p->~_Up();
+        }
+
+  };
+  template<typename _Tp>
+  inline bool operator==(const __allocator_base<_Tp>&, const __allocator_base<_Tp>&){
+      return true;
+  }
+  template<typename _Tp>
+  inline bool operator!=(const __allocator_base<_Tp>&, const __allocator_base<_Tp>&){
+      return false;
+  }
+  template<typename _Tp1, typename _Tp2>
+  inline bool operator==(const __allocator_base<_Tp1>&, const __allocator_base<_Tp2>&){
+      return true;
+  }
+  template<typename _Tp1, typename _Tp2>
+  inline bool operator!=(const __allocator_base<_Tp1>&, const __allocator_base<_Tp2>&){
+      return false;
+  }
+}
+
+#endif
---
+++ ys_construct.hpp
@@ -0,0 +1,65 @@
+#ifndef _YS_CONSTRUCT_H
+#define _YS_CONSTRUCT_H 1
+
+#include "new"
+#include "type_traits"
+#include "ys_iterator_base_types.hpp"
+
+namespace YS_STL{
+
+  using std::ptrdiff_t;
+  using std::size_t;  
+
+  template<typename _Tp, typename... _Args>
+    inline void _construct(_Tp* _p,_Args&&... _args){
+        ::new(static_cast<void*>(_p)) _Tp(_args);
+    }
+  
+  template<typename _Tp>
+    inline void _destory(_Tp* _p){
+        _p->~Tp();
+    }
+
+  /**
+   * helper function for trivial object
+   * do nothing
+   */
+  template<bool>
+    struct _Destory_aux{
+        template<typename ForwardIterator>
+          static void __destory(ForwardIterator _first, ForwardIterator _last){}
+    };
+
+  /**
+   * specialization
+   * helper function for non-trivial object
+   * invoke the destructor
+   */
+  template<>
+    struct _Destory_aux<true>{
+        template<typename ForwardIterator>
+          static void __destory(ForwardIterator _first, ForwardIterator _last){
+              for(; _first != _last; ++_first)
+                _destory(&(*_first));
+          }
+    };
+
+  // I don't know how does __has_trivial_destructor work and where is it defined 
+  template<typename ForwardIterator>
+    inline void _destory(ForwardIterator _first, ForwardIterator _last){
+        typedef typename iterator_traits<ForwardIterator>::value_type   _value_type;
+        _Destory_aux<__has_trivial_destructor(_value_type)>::__destory(_first,_last);
+    }
+  
+  // the following two functions needn't be defined I guess
+  template<>
+    inline void _destory<char*>(char*, char*) {}
+
+#ifdef _WCHAR_T
+  template<>
+    inline void _destory<wchar_t*>(wchar_t*, wchar_t*) {}
+#endif
+
+}
+
+#endif
